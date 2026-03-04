e411c333ac25fc9e57fc8d5c00d5a326fcadbf6e
adam <afahey87@gmail.com>
2026-03-03 21:12:32 -0500

    Added more code

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
+class ReseaschResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatGroq(model="llama-3.1-8b-instant")
-response = llm.invoke("What is the capital of France?")
-print(response)
+
+parser = PydanticOutputParser(pydantic_object=ReseachResponse)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    tools=[],
+    prompt=prompt
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], Verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the current state of research on AI alignment?"})
+print(raw_response)
